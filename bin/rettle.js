63b389950a431d6d01ab5042c7cf6b0a8f37fff8
t.inoue <takayuki.inoue@i-studio.co.jp>
2023-01-29 22:37:51 +0900

    upload

--- bin/rettle.js
+++ bin/rettle.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.defineOption = void 0;
+var defineOption = function (option) {
+    return option;
+};
+exports.defineOption = defineOption;
---
+++ src/rettle.ts
@@ -0,0 +1,3 @@
+export const defineOption = (option: RettleConfigInterface) => {
+  return option;
+}
