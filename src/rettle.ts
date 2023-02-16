ee6d8ab72f3c049c853a2b81f2e979e72fe95205
t.inoue <takayuki.inoue@i-studio.co.jp>
2023-02-16 23:50:43 +0900

    #5 修正

--- src/rettle.ts
+++ src/rettle.ts
@@ -1,5 +1,15 @@
 import {RettleConfigInterface} from "./utils/config";
+import {default as emotionCreateCache, EmotionCache} from "@emotion/cache";
+import ReactDom from "react-dom/server";
+import createEmotionServer from "@emotion/server/create-instance";
 
 export const defineOption = (option: Partial<RettleConfigInterface>) => {
   return option;
+}
+
+export const createCache = (key: string) => emotionCreateCache({key});
+
+export const createRettle = (cache: EmotionCache, element: JSX.Element) => {
+  const {extractCritical } = createEmotionServer(cache);
+  return extractCritical(ReactDom.renderToString(element));
 }
