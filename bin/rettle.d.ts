3de911a16a47a72d3d3fcfe72e4840caeafa9489
t.inoue <takayuki.inoue@i-studio.co.jp>
2023-02-20 01:23:11 +0900

    no issue -> fix package name

--- bin/rettle.d.ts
+++ bin/rettle.d.ts
@@ -4,3 +4,5 @@ import { EmotionCache } from "@emotion/cache";
 export declare const defineOption: (option: Partial<RettleConfigInterface>) => Partial<RettleConfigInterface>;
 export declare const createCache: (key: string) => EmotionCache;
 export declare const createRettle: (cache: EmotionCache, element: JSX.Element) => import("@emotion/server/create-instance").EmotionCritical;
+export declare const getRefs: (hash: string) => void | Record<string, Element | HTMLElement>;
+export declare const getRef: <T>(hash: string, key: string) => T;
