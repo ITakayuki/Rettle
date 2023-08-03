894139bf677d9e78cdf7133c3fadde521e4f0421
t.inoue <takayuki.inoue@i-studio.co.jp>
2023-08-03 20:03:35 +0900

    versions/0.0.8-beta3.0

---
+++ bin/utils/HTMLBuilder.d.ts
@@ -0,0 +1,17 @@
+export declare const transformReact2HTMLCSS: (path: string) => Promise<{
+    html: string;
+    ids: Array<string>;
+    css: string;
+}>;
+export declare const createHeaderTags: (tagName: string, contents: Record<string, string | number | boolean>[]) => string[];
+export declare const createHeaders: () => string[];
+interface RettleHelmetType {
+    headers: string[];
+    attributes: {
+        body: string;
+        html: string;
+    };
+    body: string[];
+}
+export declare const createHelmet: () => RettleHelmetType;
+export {};
---
+++ bin/utils/config.d.ts
@@ -0,0 +1,45 @@
+/// <reference types="node" />
+import { templateHTMLInterface } from "./template.html";
+import * as esBuild from "esbuild";
+import js_beautify from "js-beautify";
+interface BuildOptionsInterface {
+    copyStatic?: () => void;
+    buildScript?: (outDir: string) => void;
+    buildCss?: (code: string, outDir: string) => string | Buffer;
+    buildHTML?: (code: string, outDir: string) => string | Buffer;
+}
+interface esbuildInterface {
+    plugins?: (mode: "server" | "client") => esBuild.Plugin[];
+}
+interface BeautifyOptions {
+    css?: js_beautify.CSSBeautifyOptions | boolean;
+    html?: js_beautify.HTMLBeautifyOptions | boolean;
+    script?: js_beautify.JSBeautifyOptions | boolean;
+}
+export interface RettleConfigInterface {
+    pathPrefix: string;
+    css: string;
+    js: string;
+    beautify: BeautifyOptions;
+    endpoints: Array<string>;
+    static: string;
+    outDir: string;
+    define?: Record<string, string>;
+    header?: {
+        meta?: Record<string, string | number | boolean>[];
+        link?: Record<string, string | number | boolean>[];
+        script?: Record<string, string | number | boolean>[];
+    };
+    template: (options: templateHTMLInterface) => string;
+    build: BuildOptionsInterface;
+    esbuild: esbuildInterface;
+    version: boolean;
+    server: {
+        port?: number;
+        host?: string;
+        listenDir?: string[];
+    };
+}
+export declare const getIgnores: (endpoint: string) => string[];
+export declare const config: RettleConfigInterface;
+export {};
